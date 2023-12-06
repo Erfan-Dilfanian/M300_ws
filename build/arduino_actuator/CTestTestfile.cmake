4ba2ca44de58448ed0e642685d74621ac4102f85
erfan900 <123599653+erfan900@users.noreply.github.com>
2023-12-05 21:57:52 -0500

    Delete build directory

--- build/arduino_actuator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/erfan/M300_ws/src/arduino_actuator
-# Build directory: /home/erfan/M300_ws/build/arduino_actuator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/forest_fire_detection_system/CMakeFiles/forest_fire_detection_system_geneus.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/forest_fire_detection_system_geneus.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
