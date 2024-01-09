28ae337d70b2ba8815d725107fb74a333068ff1b
erfan900 <erfan.dilfanian@gmail.com>
2024-01-09 00:01:25 -0500

    release time test

--- src/forest_fire_detection_system/src/app/InMotionDropping/InMotionDropping_Flight_control_node.cpp
+++ src/forest_fire_detection_system/src/app/InMotionDropping/InMotionDropping_Flight_control_node.cpp
@@ -615,7 +615,7 @@ void velocityAndYawRateCtrl(const JoystickCommand &offsetDesired, uint32_t timeM
   {
     currentTime = ros::Time::now().toSec();
     elapsedTimeInMs = (currentTime - originTime) * 1000;
-      ROS_INFO("timeinMs [%f]",elapsedTimeInMs);
+      ROS_INFO("timeinMs [%d]",elapsedTimeInMs);
       joystick_action_client.call(joystickAction);
     if(elapsedTimeInMs>1000){
         ROS_INFO("release valve");
