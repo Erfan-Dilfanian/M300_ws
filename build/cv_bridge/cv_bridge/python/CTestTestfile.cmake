ed6a08383954a4c9e8ce48f3201851d0c1109799
Erfan-Dilfanian <123599653+Erfan-Dilfanian@users.noreply.github.com>
2024-04-08 18:44:36 -0400

    Delete build directory

--- build/cv_bridge/cv_bridge/python/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/erfan/M300_ws/src/cv_bridge/cv_bridge/python
-# Build directory: /home/erfan/M300_ws/build/cv_bridge/cv_bridge/python
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
